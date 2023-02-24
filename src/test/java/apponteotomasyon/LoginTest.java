4da7d7377881d2efde3834b19f1381000e387ccc
Hayrullah DEDELER <h.dede515151@gmail.com>
2023-02-24 22:54:18 +0300

    day05/1

--- src/test/java/apponteotomasyon/LoginTest.java
+++ src/test/java/apponteotomasyon/LoginTest.java
@@ -20,6 +20,8 @@ public class LoginTest {
     public AppiumDriver<WebElement>  driver;
     public WebDriverWait wait;
 
+    String phoneNumber = "5411303151";
+
     By consultantButton = By.id("mobi.appcent.apponte:id/btnInstitutional");
 
     By loginButton = By.id("mobi.appcent.apponte:id/ibSignIn");
@@ -28,6 +30,21 @@ public class LoginTest {
 
     By sendCodeButton = By.id("mobi.appcent.apponte:id/btnSendCode");
 
+    By otpSendCodeButton = By.id("mobi.appcent.apponte:id/btnOTPContinue");
+
+    By addClientButton = By.id("mobi.appcent.apponte:id/appointmentFragmentIcon");
+
+    By addPersonButton = By.id("mobi.appcent.apponte:id/profileEditImgBox");
+
+    By nameText = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText");
+
+    By surnameText = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText");
+
+    By phoneText = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText");
+
+    By clientSaveButton = By.id("mobi.appcent.apponte:id/btnSave");
+
+    By searchText = By.id("mobi.appcent.apponte:id/search_src_text");
 
 
 
@@ -46,9 +63,9 @@ public class LoginTest {
             cap.setCapability("udid","0772625218103071");
             cap.setCapability("platformVersion","12.0");
             cap.setCapability("appPackage","mobi.appcent.apponte");
-            cap.setCapability("appActivity","mobi.appcent.apponte.ui.activity.login.LoginActivity");
+            cap.setCapability("appActivity","mobi.appcent.apponte.ui.activity.home.HomeActivity");
             cap.setCapability("skipUnlock","true");
-            cap.setCapability("noReset","false");
+            cap.setCapability("noReset","true");
 
             driver = new AndroidDriver (new URL("http://127.0.0.1:4723/wd/hub"),cap);
             wait = new WebDriverWait(driver,10);
@@ -73,13 +90,65 @@ public class LoginTest {
 
         driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
         WebElement phoneNumberSelect = driver.findElement(numberText);
-        driver.getKeyboard().pressKey("5551112233");
+        driver.getKeyboard().pressKey("5411303151");
 
         driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
         WebElement sendCodeSelect = driver.findElement(sendCodeButton);
         sendCodeSelect.click();
 
         Thread.sleep(10000);
+
+        WebElement otpSendCodeSelect = driver.findElement(otpSendCodeButton);
+        otpSendCodeSelect.click();
+
+        Thread.sleep(10000);
+
+    }
+
+    @Test
+    public void addClient() throws InterruptedException {
+
+        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+
+        WebElement addClientSelect = driver.findElement(addClientButton);
+        addClientSelect.click();
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+        WebElement clientaddSelect = driver.findElement(addPersonButton);
+        clientaddSelect.click();
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+        WebElement nameTextSelect = driver.findElement(nameText);
+        // First we click the text box because we want to open keyword, so we can send keys
+        nameTextSelect.click();
+        driver.getKeyboard().sendKeys("Hayrullah");
+
+        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+
+        WebElement surnameTextSelect = driver.findElement(surnameText);
+        surnameTextSelect.click();
+        driver.getKeyboard().sendKeys("Dedeler");
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+        WebElement phoneTextSelect = driver.findElement(phoneText);
+        phoneTextSelect.click();
+        driver.getKeyboard().pressKey(phoneNumber);
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+        WebElement clientSaveSelect = driver.findElement(clientSaveButton);
+        clientSaveSelect.click();
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+        WebElement searchTextSelect = driver.findElement(searchText);
+        searchTextSelect.click();
+        driver.getKeyboard().sendKeys("H");
+
+        Thread.sleep(2000);
 
 
 
@@ -91,5 +160,6 @@ public class LoginTest {
 
     public void teardown(){
 
+        driver.quit();
     }
 }
